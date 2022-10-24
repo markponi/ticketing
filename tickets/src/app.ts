c82ff535b500f998332b25761237325454dda6f7
markponi <markjponiatowski@gmail.com>
2022-10-23 21:06:49 -0400

    updated mongo_URI env variable

--- tickets/src/app.ts
+++ tickets/src/app.ts
@@ -4,10 +4,7 @@ import { json } from 'body-parser';
 import cookieSession from 'cookie-session';
 import { errorHandler, NotFoundError } from '@markponitickets/common';
 
-import { currentUserRouter } from './routes/current-user';
-import { signinRouter } from './routes/signin';
-import { signoutRouter } from './routes/signout';
-import { signupRouter } from './routes/signup';
+
 
 const app = express();
 app.set('trust proxy', true);
@@ -19,11 +16,6 @@ app.use(
     secure: process.env.NODE_ENV !== 'test',
   })
 );
-
-app.use(currentUserRouter);
-app.use(signinRouter);
-app.use(signoutRouter);
-app.use(signupRouter);
 
 app.all('*', async (req, res, next) => {
   throw new NotFoundError();
